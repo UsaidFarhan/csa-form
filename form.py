17758028a039e322f1b23a35049ed326f0d72038
UsaidFarhan <63914093+UsaidFarhan@users.noreply.github.com>
2025-09-30 10:34:53 +0500

    Update form.py

--- form.py
+++ form.py
@@ -10,7 +10,7 @@ from docx import Document
 GEMINI_API_KEY = st.secrets["API-KEY"]
 
 # Gemini API endpoint
-GEMINI_MODEL_NAME = "models/gemini-1.5-flash-002"  # Flash model
+GEMINI_MODEL_NAME = "models/gemini-1.5-flash"  # Flash model
 GEMINI_API_URL = f"https://generativelanguage.googleapis.com/v1/{GEMINI_MODEL_NAME}:generateContent"
 
 # Function to call Gemini API
@@ -172,6 +172,7 @@ if 'extracted_dict' in st.session_state:
             st.success("CSA Form generated successfully!")
         except Exception as e:
             st.error(f"Error generating CSA form: {e}")
+
 
 
 
